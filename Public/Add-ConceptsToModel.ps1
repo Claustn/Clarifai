16737508a0b72542044dac4f0539d0d030e99f84
Claus T Nielsen <claustn@gmail.com>
2017-05-01 16:09:29 +0200

    Added Add-ConceptsToModel and Get-Model

---
+++ Public/Add-ConceptsToModel.ps1
@@ -0,0 +1,59 @@
+﻿function Add-ConceptsToModel
+{
+  [CmdletBinding()]
+  param
+  (
+    [string]$ModelName,
+    [string]$Token = (Get-ClarifaiToken),
+    [string[]]$Concepts
+
+  )
+	
+
+  $uri = 'https://api.clarifai.com/v2/models' 
+	
+  $headers = @{
+    Authorization = 'Bearer {0}' -f $Token
+    Accept        = 'application/json'
+    'Content-Type' = 'application/json'
+  }
+	
+
+
+  $Cons = @()
+  Foreach ($Concept in $Concepts) 
+  {
+    $Cons += New-Object -TypeName PSObject -Property  @{
+      id = $Concept
+    }
+  }
+  	
+  $jsonbody = [ordered]@{
+    models = @([ordered]@{
+        id          = $ModelName
+        output_info = [ordered]@{
+          data = @{
+            concepts = @(
+              $Cons          
+            )
+          }
+        }
+    })
+    action = 'merge'
+  } | ConvertTo-Json -Depth 6
+  
+	
+  $jsonbody
+  Try 
+  {
+    $Res = Invoke-RestMethod -Uri $uri -Body $jsonbody -Headers $headers -Method Patch -ErrorAction Stop
+    $Res
+  }
+  Catch 
+  {    
+    $Err = $($_.ErrorDetails.Message| ConvertFrom-Json)
+    Write-Host  -Object "$($Err.inputs.status.Description) "    
+    Throw $_
+  }
+}
+
---
+++ Public/Get-Model.ps1
@@ -0,0 +1,31 @@
+﻿function Get-Model
+{
+  [CmdletBinding()]
+  param
+  (
+  [string]$Token = (Get-ClarifaiToken) 
+  )
+	
+  
+	
+  $uri = 'https://api.clarifai.com/v2/models' 
+	
+  $headers = @{
+    Authorization = 'Bearer {0}' -f $Token
+    Accept        = 'application/json'
+    'Content-Type' = 'application/json'
+  }
+
+
+  Try 
+  {
+    $Res = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get -ErrorAction Stop
+    $Res.models
+  }
+  Catch 
+  {    
+    $Err = $($_.ErrorDetails.Message| ConvertFrom-Json)
+    Write-Host  -Object "$($Err.inputs.status.Description) "    
+    Throw $_
+  }
+}
